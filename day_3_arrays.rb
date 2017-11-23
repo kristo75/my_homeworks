7360db472a39743c7539aa7c26bbbda6385ef0c3
Kris <kristopher_anderson@icloud.com>
2017-11-23 10:05:32 +0000

    Wednesday Homework

--- day_3_arrays.rb
+++ day_3_arrays.rb
@@ -37,23 +37,6 @@ stops[3] = "Polmont"
 # A. Given the following data structure:
 
 
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-
-
-# 1. Add `"Edinburgh Waverley"` to the end of the array
-# 2. Add `"Glasgow Queen St"` to the start of the array
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-# 4. Work out the index position of `"Linlithgow"`
-# 5. Remove `"Livingston"` from the array using its name
-# 6. Delete `"Cumbernauld"` from the array by index
-# 7. How many stops there are in the array?
-stops.count
-# 8. How many ways can we return `"Falkirk High"` from the array?
-# 9. Reverse the positions of the stops in the array
-# 10. Print out all the stops using a for loop
-
-## B. Given the following data structure:
-
   users = {
     "Jonathan" => {
       :twitter => "jonnyt",
--- wednesday_homework.rb
+++ wednesday_homework.rb
@@ -7,17 +7,52 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #
 # # 1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
+# stops.push("Edinburgh Waverley")
+# p stops
+
 # # 2. Add `"Glasgow Queen St"` to the start of the array
-# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.unshift("Glasgow Queen Street")
+# p stops
+
+# # # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.insert(4, "polmont")
+# p stops
+
 # # 4. Work out the index position of `"Linlithgow"`
-# # 5. Remove `"Livingston"` from the array using its name
-# # 6. Delete `"Cumbernauld"` from the array by index
-# # 7. How many stops there are in the array?
-# # 8. How many ways can we return `"Falkirk High"` from the array?
-# # 9. Reverse the positions of the stops in the array
-# # 10. Print out all the stops using a for loop
+# p stops.index("Linlithgow")
+
+
+# # # 5. Remove `"Livingston"` from the array using its name
+#stops.delete("Livingston")
+
+
+# # # 6. Delete `"Cumbernauld"` from the array by index
+# stops.slice!(2)
 #
+# OR
+#
+# stops.delete_at(2)
+
+# # # 7. How many stops there are in the array?
+# p stops.lenth
+# p stops.countries
+# p stops.size
+
+# # # 8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2]
+# p stops[-5]
+# p stops.fetch(2)
+# p stops.slice(2)
+# p stops.at(2)
+
+# # 9. Reverse the positions of the stops in the array
+# p stops.reverse
+
+# # # 10. Print out all the stops using a for loop
+# for stop in stops
+#   p stop
+# end
+
 # ## B. Given the following data structure:
 #
 #   users = {
@@ -52,16 +87,45 @@ stops.push("Edinburgh Waverley")
 #     },
 #   }
 #
-# # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# # 2. Get Erik's hometown
-# # 3. Get the array of Erik's favourite numbers
-# # 4. Get the type of Avril's pet Colin
-# # 5. Get the smallest of Erik's favourite numbers
-# # 6. Add the number `7` to Erik's favourite numbers
-# # 7. Change Erik's hometown to Edinburgh
-# # 8. Add a pet dog to Erik called "Fluffy"
-# # 9. Add yourself to the users hash
+# # # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# # p users["Jonatha"] [:twitter]
 #
+# # # 2. Get Erik's hometown
+# p users ["Erik"] [:home_town]
+#
+# # # 3. Get the array of Erik's favourite numbers
+# p users ["Erik"] [:favourite_numbers]
+#
+# # # 4. Get the type of Avril's pet Colin
+# p users ["Avril"] [:pets] ["Colin"]
+
+# # 5. Get the smallest of Erik's favourite numbers
+# p users ["Erik"] [:favourite_numbers].sort.first
+
+# # 6. Add the number `7` to Erik's favourite numbers
+# p users ["Erik"] [:favourite_numbers].push(7)
+
+# # # 7. Change Erik's hometown to Edinburgh
+# p users ["Erik"] [:home_town] = "Edinburgh"
+# p users ["Erik"]
+
+# # 8. Add a pet dog to Erik called "Fluffy"
+# users ["Erik"] [:pets] ["Fluffy"] = :dog
+
+# # # 9. Add yourself to the users hash
+# new_user ={
+#   :twitter => "topher75",
+#   #       :favourite_numbers => [12, 14, 85, 88],
+#   #       :home_town => "Dunbar",
+#   #       :pets => {
+#   #         "colin" => :snake
+#   #       }
+#   #     }
+#
+#
+# users["kris"] = new_user
+# p users
+
 # # C. Given the following data structure:
 #
 #
@@ -83,6 +147,23 @@ stops.push("Edinburgh Waverley")
 #
 #
 # # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
-# # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
-# # 3. Use a loop to print the names of all the countries in the UK.
-# # 4. Use a loop to find the total population of the UK.
+# united_kingdon[1] [:capital] = "Cardiff"
+#
+# # # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+# norther_ireland = {
+#   name: "Norther Ireland",
+#   captal: "Belfast",
+#   population: 1811000
+# }
+# united_kingdom.push(norther_ireland)
+
+# # # 3. Use a loop to print the names of all the countries in the UK.
+# for country in united_kingdom
+# #   p country[:name]
+#
+# # # 4. Use a loop to find the total population of the UK.
+# for country in united_kingdom
+#   population_total += country[:population]
+#   end
+#   p population_total
+#
